261582bcb6381210cbac25ae00317b47bb67a92d
小芝 涼太 <KoshibaRyouta@oshiba-no-MacBook-Pro.local>
2015-03-12 01:51:40 +0900

    四則演算できる段階

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,17 +9,111 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number1 = 0.0,number2=0.0,number3=0.0
+    var ope:Int = 0
+    
+    let myLabel : UILabel = UILabel(frame : CGRectMake( 130, 50, 150, 40))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.cyanColor()
+        self.view.addSubview(myLabel)
+        myLabel.backgroundColor = UIColor.orangeColor()
+        myLabel.text = "0"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func button1(sender: AnyObject){
+        bunki(1)
+    }
+    
+    @IBAction func button2(sender: AnyObject){
+        bunki(2)
+    }
+    
+    @IBAction func button3(sender: AnyObject){
+        bunki(3)
+    }
+    
+    @IBAction func button4(sender: AnyObject){
+        bunki(4)
+    }
+    
+    @IBAction func button5(sender: AnyObject){
+        bunki(5)
+    }
+    
+    @IBAction func button6(sender: AnyObject){
+        bunki(6)
+    }
+    
+    @IBAction func button7(sender: AnyObject){
+        bunki(7)
+    }
+    
+    @IBAction func button8(sender: AnyObject){
+        bunki(8)
+    }
+    
+    @IBAction func button9(sender: AnyObject){
+        bunki(9)
+    }
+    
+    @IBAction func button0(sender: AnyObject){
+        bunki(0)
+    }
+    
+    @IBAction func plus(sender: AnyObject){
+        ope=1
+    }
+    @IBAction func minus(sender: AnyObject){
+        ope=2
+    }
+    @IBAction func kakeru(sender: AnyObject){
+        ope=3
+    }
+    @IBAction func waru(sender: AnyObject){
+        ope=4
+    }
+    @IBAction func clear(sender: AnyObject){
+        number1=0.0
+        number2=0.0
+        number3=0.0
+        ope=0
+        myLabel.text="0.0"
+    }
+    @IBAction func equal(sender: AnyObject){
+        switch ope{
+        case 1:
+            number3=number1+number2
+        case 2:
+            number3=number1-number2
+        case 3:
+            number3=number1*number2
+        case 4:
+            number3=number1/number2
+        default:
+            break
+        }
+        myLabel.text = "\(number3)"
+    }
+    
+    func bunki (i:Double){
+        if(ope==0){
+            number1=number1*10+i
+            myLabel.text = "\(number1)"
+        }else{
+            number2=number2*10+i
+            myLabel.text = "\(number2)"
+        }
+    }
 
 }
 
